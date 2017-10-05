c999555c0e8ed9ac7e17a40f3cb4067202c1e200
abeoWTF <31953595+abeoWTF@users.noreply.github.com>
2017-10-05 11:39:20 +0200

    Final upload, added highscore.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ namespace Labb4_enLitenUpdate
     {
         static void Main(string[] args)
         {
+
+            /*Highscore: 
+            * 1. Jeppe - 64 moves. 1850 points.
+            * 2. Silvija - 122 moves, 575 points.
+            * 3. Markus - 100 moves, 300 points.
+            * 4. 
+            * 
+            * 
+            * */
             //Skapande av kartklass inklusive initering av tecknen i kartan
             Map m = new Map();
 
@@ -16,18 +25,7 @@ namespace Labb4_enLitenUpdate
             m.UpdateMap();
 
             Console.ReadKey();
-
-
         }
 
     } //class Program ends here
-
-
-
-
-
-
-
-
-
 }  //EOF
